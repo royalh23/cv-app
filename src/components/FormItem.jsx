4caa852088af6ef6b26bd9c8c5389c319f291587
Royal <royalhuseyn902@gmail.com>
2024-05-16 17:20:38 +0400

    Fix img issues Exlucde svg ending in dev mode because it changes during prod

--- src/components/FormItem.jsx
+++ src/components/FormItem.jsx
@@ -8,14 +8,12 @@ export default function FormItem({ title, onEditClick, onDeleteClick, id }) {
       <h3>{title}</h3>
       <div className="icons">
         <img
-          className="smaller-icon"
           src={EditIcon}
           alt="Edit icon"
           onClick={onEditClick}
           data-id={id}
         />
         <img
-          className="smaller-icon"
           src={DeleteIcon}
           alt="Delete icon"
           onClick={onDeleteClick}
